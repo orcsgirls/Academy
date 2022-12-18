be9507f1dd80c9ceac1f5422abd728c4c6af5dc5
Thomas Proffen <thomasproffen@gmail.com>
2022-12-18 14:10:31 -0500

    Cleanup on liquid thermometer examples

--- RaspberryPico/codeLiquidTClass.py
+++ RaspberryPico/codeLiquidTClass.py
@@ -3,54 +3,42 @@ import board
 from adafruit_onewire.bus import OneWireBus
 from adafruit_ds18x20 import DS18X20
 
-#-------------------------------------------------------------------------
-# This is out liquidThermometer class which inherits from the 
-# DS18X20 class used for our liquids thermometer.
-#-------------------------------------------------------------------------
-class liquidThermometer(DS18X20):
+class MyDS18X20(DS18X20):
 
-    def __init__(self, pin, unit='c'):
-        self.pin  = pin
-        self.ow_bus = OneWireBus(self.pin)
-        super().__init__(self.ow_bus, self.ow_bus.scan()[0])
+    def __init__(self, pin):
+        ow_bus = OneWireBus(pin)
+        super().__init__(ow_bus, ow_bus.scan()[0])
+        self.unit='C'
+        self.min=9999.  # Hope the first reading is smaller than 9999 :)
+        self.max=-9999. # Hope the first reading is larger than -9999 :)
+        print("Init done :)")
 
-        self.unit = unit        
-        self.max = -9999
-        self.min = 9999
-
-    def _updateMaxMin(self, value):
-        self.max = max(self.max,value)
-        self.min = min(self.min,value)            
-    
     def reset(self):
-        self.max = -9999
-        self.min = 9999
-        
+        self.max=-9999.
+        self.min=9999.
+
     @property
     def temperature(self):
         c = super().temperature
         if (self.unit=='C'):
-            self._updateMaxMin(c)
+            self.max=max(self.max,c) # Make self.max the larger value
+            self.min=min(self.min,c) # Make self.max the larger value
             return c
         elif (self.unit=='F'):
             f = c * 1.8 + 32.0
-            self._updateMaxMin(f)
+            self.max=max(self.max,f) # Make self.max the larger value
+            self.min=min(self.min,f) # Make self.max the larger value
             return f
         else:
             return 'Invalid unit'
 
-#-------------------------------------------------------------------------
-# Instantiating the class
-#-------------------------------------------------------------------------
+sensor=MyDS18X20(board.GP16)
+sensor.unit='F'   # Setting if to F
 
-t = liquidThermometer(board.GP22, "F")
-print (f"Thermometer connected. Unit is {t.unit}")
-
-#-------------------------------------------------------------------------
-# Measuring loop
-#-------------------------------------------------------------------------
-
-while True:
-    print(f"Temperature: {t.temperature:.3f} {t.unit} - Max: {t.max:.3f} {t.unit}, Min: {t.min:.3f} {t.unit}")
+# Getting 20 values
+for i in range(20):
+    print(f"Temperature: {sensor.temperature:0.3f}{sensor.unit}")
     time.sleep(1.0)
 
+# Printing max and min values measured
+print(f"Maximum: {sensor.max:.3f}{sensor.unit}, Minimum: {sensor.min:.3f}{sensor.unit}")
--- RaspberryPico/codeLiquidThermometer.py
+++ RaspberryPico/codeLiquidThermometer.py
@@ -1,21 +1,12 @@
 import time
 import board
 from adafruit_onewire.bus import OneWireBus
-import adafruit_ds18x20
+from adafruit_ds18x20 import DS18X20
 
 # This uses a socalled one-wire protocol
 ow_bus = OneWireBus(board.GP16)
-devices = ow_bus.scan()
-
-# List all devices - we are looking for Family 0x28. If it is
-# not the first one, we need to chance devices[0] to the right one.
-
-for device in devices:
-    print("ROM = {} \tFamily = 0x{:02x}".format([hex(i) for i in device.rom], device.family_code))
-
-# Now we create the temperature object
-ds18b20 = adafruit_ds18x20.DS18X20(ow_bus, devices[0])
+sensor = DS18X20(ow_bus, ow_bus.scan()[0])
 
 while True:
-    print('Temperature: {0:0.3f} °C'.format(ds18b20.temperature))
+    print(f"Temperature: {sensor.temperature:0.3f} °C")
     time.sleep(1.0)
